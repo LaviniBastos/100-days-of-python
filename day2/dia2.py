b8f3c6a92ff19de19951b1d1f496a7ed392298d7
LaviniBastos <160741212+LaviniBastos@users.noreply.github.com>
2025-04-18 15:52:00 -0300

    Add files via upload

---
+++ day2/dia2.py
@@ -0,0 +1,16 @@
+name = 'Lavini'
+idade = 29
+altura = 1.86
+estudante = True
+email = None
+apelidos = ["Lavi", "ni"]
+parque_favorito = {"nome": "Villa-Lobos", "endereço": "Pinheiros" }
+
+print(f"nome: {name}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
+print(f"Estudando: {estudante}")
+print(f"Email: {email}")
+print(f"Apelidos: {apelidos}")
+print(f"Parque Favorito: {parque_favorito}")
+      
