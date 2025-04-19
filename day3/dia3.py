1f2c849faf10f1b749e50b546cb6f7f10359acd1
LaviniBastos <160741212+LaviniBastos@users.noreply.github.com>
2025-04-19 15:49:54 -0300

    Add files via upload

---
+++ day3/dia3.py
@@ -0,0 +1,8 @@
+name = input("Digite aqui seu nome")
+print(f"Oláa, {name}")
+
+age = int(input("sua idade"))
+altura = float(input("Sua altura"))
+
+print(f"Sua altura é: {altura}")
+print(f"Sua idade é: {age}")
