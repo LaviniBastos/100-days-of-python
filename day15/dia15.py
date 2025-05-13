f9d1186a80409708f03f04bc2fba0a0105253ca7
LaviniBastos <160741212+LaviniBastos@users.noreply.github.com>
2025-05-13 10:05:50 -0300

    Add files via upload

---
+++ day15/dia15.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero para descobrir se ele é par ou impar: "))
+
+if numero % 2 == 0:
+    print(f"O número {numero} é par")
+else:
+    print(f"O numero {numero} é impar: ")
