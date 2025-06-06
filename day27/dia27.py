3dc98c1f8f39267147d16d814582e2afec505c0a
LaviniBastos <160741212+LaviniBastos@users.noreply.github.com>
2025-06-06 16:48:02 -0300

    Add files via upload

---
+++ day27/dia27.py
@@ -0,0 +1,7 @@
+def palavras_para_frases(palavras):
+    return ' '.join(palavras)
+
+
+#Testando
+lista_palavras = ["Python", "é", "um", "universo", "incrível"]
+print(palavras_para_frases(lista_palavras))
