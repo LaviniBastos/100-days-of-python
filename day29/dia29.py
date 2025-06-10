e4d8759754c4a2f0fee87ec78ba27d0f721474a0
LaviniBastos <160741212+LaviniBastos@users.noreply.github.com>
2025-06-10 15:28:41 -0300

    Add files via upload

---
+++ day29/dia29.py
@@ -0,0 +1,7 @@
+def ordenar_lista(lista):
+    return sorted(lista)
+
+
+print(ordenar_lista([1, 5, 6, 3, 5]))
+print(ordenar_lista([80, 40, 60, 20, 33, 90, 88]))
+print(ordenar_lista([78, 90, 334, 39, 90, 1, 4]))
